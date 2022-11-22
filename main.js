0273f8be42afdd9108226c0b9900a5329739e7ae
kee_hoon <djfkfk12345@naver.com>
2022-11-22 13:45:42 +0900

    221122생활코딩

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 11장 부터
+// 16장 부터
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -6,15 +6,42 @@ var url = require('url');
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title = queryData.id
     console.log(queryData.id);
     if(_url == '/'){
-        _url = '/index.html';
+        title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(queryData.id); // __dirname : 현재 디렉토리 위치, url : 실행중인 파일의 이름
+    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>
+            ${description}
+            </p>
+        </body>
+        </html>
+        `;
+        response.end(template);
+    });
+
+     // __dirname : 현재 디렉토리 위치, url : 실행중인 파일의 이름
     
 });
+
 app.listen(3000);
