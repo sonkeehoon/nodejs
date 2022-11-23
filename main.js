4bf5a9d016567efd20b2f1450711ef33704714fb
kee_hoon <djfkfk12345@naver.com>
2022-11-23 21:40:29 +0900

    221123

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 16장 부터
+// 20장 부터
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -6,39 +6,69 @@ var url = require('url');
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id
-    console.log(queryData.id);
-    if(_url == '/'){
-        title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });    
+        }
+        
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>
-            ${description}
-            </p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
+
+
+
 
      // __dirname : 현재 디렉토리 위치, url : 실행중인 파일의 이름
     
--- nodejs/fileread.js
+++ nodejs/fileread.js
@@ -2,3 +2,12 @@ var fs = require('fs');
 fs.readFile('sample.txt', 'utf-8', function(err, data){
     console.log(data);
 });
+
+var args = process.argv;
+console.log(args[2]);
+
+if (args[2] === '1'){
+    console.log("1을 입력하셨군요");
+} else {
+    console.log("1이 아닌것을 입력하셨군요");
+}
