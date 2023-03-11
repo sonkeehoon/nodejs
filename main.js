2ff8b892f34d5d23cca196fd2de2051105eeffb5
kee_hoon <djfkfk12345@naver.com>
2023-03-11 14:27:09 +0900

    230311

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 12강 -> 수업의 정상
 // https://www.youtube.com/watch?v=VToaABEsfbY&list=PLuHgQVnccGMAicFFRh8vFFFtLLlNojWUh&index=11
 // 27강 - 수업의 정상(초보자는 여기까지만)
-// Node.js & MySQL 15강부터
+// Node.js & MySQL 17강부터
 // arr.push : 파이썬의 리스트append와 동일 기능
 // require는 파이썬의 import와 비슷한 의미이다 (모듈을 가져옴)
 var port = 5000;
@@ -9,25 +9,20 @@ var port = 5000;
 
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
 // 리팩토링은 중요하다
-var template = require('./lib/template.js');
-var db = require('./lib/db.js');
-var topic = require('./lib/topic')
+var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
     if(pathname === '/'){
         if(queryData.id === undefined){
             topic.home(request, response);
-
         } else {
             topic.page(request, response);
         }
-        
     } else if(pathname === '/create'){
         topic.create(request, response);
     } else if(pathname === '/create_process') {
@@ -38,7 +33,9 @@ var app = http.createServer(function(request, response){
         topic.update_process(request, response);
     } else if(pathname === '/delete_process'){
         topic.delete_process(request, response);
-    } else {
+    } else if(pathname === '/author'){
+        author.home(request, response);
+    }else {
         response.writeHead(404);
         response.end('Not found');
     }
